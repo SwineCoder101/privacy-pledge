b75720870ad4ffceb01254493def7405167d1064
aeither <36173828+aeither@users.noreply.github.com>
2023-11-11 22:48:14 +0300

    ui works

--- ui/src/pages/index.page.tsx
+++ ui/src/pages/index.page.tsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from "react";
-import "./reactCOIServiceWorker";
-import ZkappWorkerClient from "./zkappWorkerClient";
-import { PublicKey, Field } from "o1js";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Progress } from "@/components/ui/progress";
-import React from "react";
+import { Field, PublicKey } from "o1js";
+import React, { useEffect, useState } from "react";
+import "./reactCOIServiceWorker";
+import ZkappWorkerClient from "./zkappWorkerClient";
 
 let transactionFee = 0.1;
 
@@ -13,6 +12,20 @@ let transactionFee = 0.1;
 const zkappPublicKey = PublicKey.fromBase58(
   "B62qpEozJn3aH6UQgwW4VYoK7fzriQx1SAaFptEtc3AfyeB2vc65Dmf"
 );
+
+const requestDatas = [
+  {
+    id: 1,
+    title: "Card 1",
+    description: "This is the description for Card 1",
+  },
+  {
+    id: 2,
+    title: "Card 2",
+    description: "This is the description for Card 2",
+  },
+  // Add more dummy data as needed
+];
 
 export default function Home() {
   const [state, setState] = useState({
@@ -261,12 +274,10 @@ export default function Home() {
   );
 
   let setup = (
-    <div
-      style={{ fontWeight: "bold", fontSize: "1.5rem", paddingBottom: "5rem" }}
-    >
+    <>
       {stepDisplay}
       {hasWallet}
-    </div>
+    </>
   );
 
   let accountDoesNotExist;
@@ -315,25 +326,37 @@ export default function Home() {
   }
 
   return (
-    <>
+    <div className="bg-gradient-to-br from-orange-50 to-yellow-100">
       <Header />
       <div className="min-h-[calc(100vh-64px)] grid grid-cols-3 p-4 gap-4 rounded">
-        <div className="col-span-1 bg-gray-200 p-4 rounded-md">
+        <div className="col-span-1 bg-card p-4 border shadow-md rounded-md">
           <FirstColumn />
         </div>
-        <div className="col-span-2 bg-gray-300 p-4 rounded-md">
-          {setup}
-          {accountDoesNotExist}
+
+        <div className="col-span-2 bg-card p-4 border shadow-md rounded-md">
+          <div className="flex w-full h-24 justify-center items-center bg-card p-2 border-2 rounded-md">
+            {setup}
+            {accountDoesNotExist}
+          </div>
+
+          {/*  */}
           {mainContent}
+          {/*  */}
+
+          <div className="grid grid-cols-2 gap-4 pt-2">
+            {requestDatas.map((item) => (
+              <CardItem key={item.id} item={item} />
+            ))}
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
 const Header = () => {
   return (
-    <header className="bg-gray-800 text-white sticky top-0">
+    <header className="border-b-2 text-black sticky top-0 bg-white">
       <div className="container mx-auto flex items-center justify-between py-4 px-6">
         <a href="#" className="text-xl font-bold">
           Privacy-Pledge
@@ -367,6 +390,21 @@ const Header = () => {
   );
 };
 
+interface Item {
+  title: string;
+  description: string;
+}
+
+const CardItem = ({ item }: { item: Item }) => {
+  return (
+    <div className="border border-gray-300 rounded-lg p-4">
+      <h3 className="text-xl font-semibold mb-2">{item.title}</h3>
+      <p className="text-gray-600 mb-4">{item.description}</p>
+      <Button className="text-white px-4 py-2 rounded-md">Vote</Button>
+    </div>
+  );
+};
+
 const FirstColumn = () => {
   const [progress, setProgress] = React.useState(13);
 
@@ -384,7 +422,7 @@ const FirstColumn = () => {
       <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
         Information
       </h2>
-      <Progress value={progress} className="w-[60%]" />
+      <Progress value={progress} />
       <div>Deposited Balance</div>
       <div>Reputation</div>
       <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
@@ -397,7 +435,7 @@ const FirstColumn = () => {
           className="border border-gray-300 rounded-md px-2 py-1 w-full"
         />
         <Button
-          className="bg-blue-500 text-white px-4 py-2 rounded-md"
+          className=" text-white px-4 py-2 rounded-md"
           onClick={handleDeposit}
         >
           Deposit
